56b4a97e423972d9a915235c78c468044b57d272
Moudou <will.cheng@mycar-in.com>
2025-02-06 15:58:33 +0800

    [Fix] SPA_ApproverSetup - 修正開啟評鑑期間後，寄送CostSerivce通知信的CC名單錯誤

--- Platform/BI.SPA_ApproverSetup/SPA_PeriodManager.cs
+++ Platform/BI.SPA_ApproverSetup/SPA_PeriodManager.cs
@@ -280,7 +280,7 @@ namespace BI.SPA_ApproverSetup
                         var srissList = this._roleMgr.GetUserListInRole(ApprovalRole.SRI_SS.ToID().Value).ToList();
                         var qsmList = this._roleMgr.GetUserListInRole(ApprovalRole.QSM.ToID().Value).ToList();
                         var receiver1_MailList = srissList.Select(obj => obj.EMail).Distinct().ToList();
-                        var cc_MailList = srissList.Select(obj => obj.EMail).Distinct().ToList();
+                        var cc_MailList = qsmList.Select(obj => obj.EMail).Distinct().ToList();
 
                         MailUtil.Send_SRISS_Mail(receiver1_MailList, cc_MailList, dbModel.Period, userID, cDate);
 
