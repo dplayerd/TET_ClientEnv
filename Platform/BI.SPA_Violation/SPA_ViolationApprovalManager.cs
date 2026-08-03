b7e39902cf2196cd8b74133a0fe1675c6d7e0f9c
nkp0704 <nkp0704@gmail.com>
2026-08-04 06:47:52 +0800

    [Dev] 供應商評鑑優化1_item8-2_供應商SPA違規紀錄資料審核完成通知信_增加CC給QSM

--- Platform/BI.SPA_Violation/SPA_ViolationApprovalManager.cs
+++ Platform/BI.SPA_Violation/SPA_ViolationApprovalManager.cs
@@ -479,6 +479,7 @@ namespace BI.SPA_Violation
                 {
                     var query =
                     from item in context.TET_SPA_ViolationApproval
+                    join user in context.Users on item.Approver equals user.UserID
                     where
                         item.ViolationID == violationId
                     orderby item.CreateDate ascending
@@ -490,7 +491,7 @@ namespace BI.SPA_Violation
                         Type = item.Type,
                         Description = item.Description,
                         Level = item.Level,
-                        Approver = item.Approver,
+                        Approver = user.FirstNameEN + " " + user.LastNameEN + " (" + item.Approver + ")",
                         Result = item.Result,
                         Comment = item.Comment,
                         CreateUser = item.CreateUser,
@@ -610,6 +611,8 @@ namespace BI.SPA_Violation
         private void SendCompleteMail(string nextLevel, SPA_ViolationModel main, SPA_ViolationApprovalModel approvalModel, string userID, DateTime cDate)
         {
             var applicant = _userMgr.GetUser(main.CreateUser);
+            var qsmList = this._roleMgr.GetUserListInRole(ApprovalRole.QSM.ToID().Value);
+            var qsmMailList = qsmList.Select(obj => obj.EMail).ToList();
             var pageUrl = $"{ModuleConfig.EmailRootUrl}/SPA_Violation/Index";
 
             EMailContent content = new EMailContent()
@@ -634,12 +637,10 @@ namespace BI.SPA_Violation
 
             foreach (var item in main.ApprovalList)
             {
-                var approverInfo = this._userMgr.GetUser(item.Approver);
-
                 content.Body +=
                 $@"
                     <tr>
-                        <td>{approverInfo.FirstNameEN} {approverInfo.LastNameEN}</td>
+                        <td>{item.Approver}</td>
                         <td>{item.Level}</td>
                         <td>{item.CreateDate.ToString("yyyy/MM/dd HH:mm:ss")}</td>
                         <td>{item.ModifyDate.ToString("yyyy/MM/dd HH:mm:ss")}</td>
@@ -651,7 +652,7 @@ namespace BI.SPA_Violation
 
             content.Body += "</table>";
 
-            MailPoolManager.WritePool(applicant.EMail, content, userID, cDate);
+            MailPoolManager.WriteMailWithCC(new List<string>() { applicant.EMail }, qsmMailList, content, userID, cDate);
         }
 
         #endregion
