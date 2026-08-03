80f01ca5a5541ad6558c66af38dccdd1da3fe45f
nkp0704 <nkp0704@gmail.com>
2026-08-04 07:06:24 +0800

    [Dev] 供應商評鑑優化1_item8-1_供應商SPA違規紀錄資料填寫審核通知信_第一關審核，增加CC給該評鑑項目: Safety、評鑑單位:EHS下面的計分資料填寫者

--- Platform/BI.SPA_Violation/Utils/ApprovalMailUtil.cs
+++ Platform/BI.SPA_Violation/Utils/ApprovalMailUtil.cs
@@ -50,8 +50,14 @@ namespace BI.SPA_Violation.Utils
         internal static void SendNewVerifyMail(UserAccountModel receiver, TET_SPA_ViolationApproval approvalModel, TET_SPA_Violation main, string userID, DateTime cDate)
         {
             UserRoleManager _roleMgr = new UserRoleManager();
+            UserManager _userMgr = new UserManager(); 
+            
             var qsmList = _roleMgr.GetUserListInRole(ApprovalRole.QSM.ToID().Value);
+            var ccList = _userMgr.GetUser(main.CreateUser);
+            
             var qsmMailList = qsmList.Select(obj => obj.EMail).ToList();
+            var ccMailList = new List<string>() { ccList.EMail };
+
             var pageUrl = $"{ModuleConfig.EmailRootUrl}/SupplierApproval/Index";
 
             EMailContent content = new EMailContent()
@@ -72,8 +78,8 @@ namespace BI.SPA_Violation.Utils
             var Email = new List<string>();
             Email.Add(receiver.EMail);
 
-            //MailPoolManager.WriteMailWithCC(Email, qsmMailList, content, userID, cDate);
-            MailPoolManager.WriteMailWithCC(Email, new List<string>(), content, userID, cDate);
+            MailPoolManager.WriteMailWithCC(Email, ccMailList, content, userID, cDate);
+            //MailPoolManager.WriteMailWithCC(Email, new List<string>(), content, userID, cDate);
         }
 
         /// <summary> 寄信新的簽核人 </summary>
