99a982b90096e648a741dec3407064d17f68a3a2
Moudou <will.cheng@mycar-in.com>
2026-08-27 15:03:37 +0800

    [Dev] DB - TET_SPA_ScoringInfo - 取消部份 Module1~Module4 的必填欄位

---
+++ DBScripts/01.01.18/01. SchemaChange.sql
@@ -0,0 +1,19 @@
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule1] ALTER COLUMN [Type] [nvarchar](16) NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule1] ALTER COLUMN [Supplier] [nvarchar](128) NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule1] ALTER COLUMN [EmpName] [nvarchar](64) NULL
+
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule2] ALTER COLUMN [MachineName] [nvarchar](64) NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule2] ALTER COLUMN [MachineNo] [nvarchar](64) NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule2] ALTER COLUMN [OnTime] [nvarchar](16) NULL
+
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule3] ALTER COLUMN [Date] [date] NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule3] ALTER COLUMN [Location] [nvarchar](64) NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule3] ALTER COLUMN [TELLoss] [nvarchar](16) NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule3] ALTER COLUMN [CustomerLoss] [nvarchar](16) NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule3] ALTER COLUMN [Accident] [nvarchar](16) NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule3] ALTER COLUMN [Description] [nvarchar](1000) NULL
+
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule4] ALTER COLUMN [Date] [date] NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule4] ALTER COLUMN [Location] [nvarchar](64) NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule4] ALTER COLUMN [IsDamage] [nvarchar](16) NULL
+ALTER TABLE [dbo].[TET_SPA_ScoringInfoModule4] ALTER COLUMN [Description] [nvarchar](1000) NULL
