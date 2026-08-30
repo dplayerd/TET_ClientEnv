e8a09c01c1a901e82a22f35e3299c05c20197523
Moudou <dplayerd@gmail.com>
2026-08-30 18:15:24 +0800

    [Fix] TET_SPA_ScoringInfoSheets - Edit - 編輯頁如果取消「勾選頁籤」時，一併將「頁籤下的所有項目」都取消勾選

--- Platform/Platform.WebSite/ModuleResources/JavaScripts/SPA_ScoringInfoSheetSetup/edit.js
+++ Platform/Platform.WebSite/ModuleResources/JavaScripts/SPA_ScoringInfoSheetSetup/edit.js
@@ -3,11 +3,65 @@ var btnSaveSelector = "#btnSave";
 
 $(document).ready(function () {
     var mainForm = $(formMain);
+    var sheetFillFieldsMap = {
+        IsSheet1Show: [
+            "IsSheet1TypeFill",
+            "IsSheet1SupplierFill",
+            "IsSheet1EmpNameFill",
+            "IsSheet1MajorJobFill",
+            "IsSheet1IsIndependentFill",
+            "IsSheet1SkillLevelFill",
+            "IsSheet1EmpStatusFill",
+            "IsSheet1TELSeniorityYFill",
+            "IsSheet1TELSeniorityMFill",
+            "IsSheet1RemarkFill"
+        ],
+        IsSheet2Show: [
+            "IsSheet2ServiceForFill",
+            "IsSheet2WorkItemFill",
+            "IsSheet2MachineNameFill",
+            "IsSheet2MachineNoFill",
+            "IsSheet2OnTimeFill",
+            "IsSheet2RemarkFill"
+        ],
+        IsSheet3Show: [
+            "IsSheet3WorkerCountFill",
+            "IsSheet3DateFill",
+            "IsSheet3LocationFill",
+            "IsSheet3TELLossFill",
+            "IsSheet3CustomerLossFill",
+            "IsSheet3AccidentFill",
+            "IsSheet3DescriptionFill"
+        ],
+        IsSheet4Show: [
+            "IsSheet4CorrectnessFill",
+            "IsSheet4ContributionFill"
+        ],
+        IsSheet5Show: [
+            "IsSheet5SelfTrainingFill",
+            "IsSheet5SelfTrainingRemarkFill"
+        ],
+        IsSheet6Show: [
+            "IsSheet6CooperationFill",
+            "IsSheet6DateFill",
+            "IsSheet6LocationFill",
+            "IsSheet6IsDamageFill",
+            "IsSheet6DescriptionFill"
+        ]
+    };
 
     setMainInput(mainForm, modelData);
+    syncAllSheetFillFields(mainForm);
+
+    Object.keys(sheetFillFieldsMap).forEach(function (showFieldName) {
+        mainForm.find("[name=" + showFieldName + "]").change(function () {
+            syncSheetFillFields(mainForm, showFieldName);
+        });
+    });
 
     $(btnSaveSelector).click(function () {
         var inputData = getMainInput(mainForm);
+        normalizeSheetFillFields(inputData);
         var msgList = [];
 
         if (!inputData.ServiceItemID) {
@@ -67,6 +121,37 @@ $(document).ready(function () {
             var item = $(this);
             var fieldName = item.prop("name");
             item.prop("checked", objFormData[fieldName] === true);
+        });
+    }
+
+    function syncAllSheetFillFields(jqObjArea) {
+        Object.keys(sheetFillFieldsMap).forEach(function (showFieldName) {
+            syncSheetFillFields(jqObjArea, showFieldName);
+        });
+    }
+
+    function syncSheetFillFields(jqObjArea, showFieldName) {
+        var isShow = jqObjArea.find("[name=" + showFieldName + "]").prop("checked");
+        var fillFields = sheetFillFieldsMap[showFieldName];
+
+        fillFields.forEach(function (fieldName) {
+            var item = jqObjArea.find("[name=" + fieldName + "]");
+
+            if (!isShow) {
+                item.prop("checked", false);
+            }
+        });
+    }
+
+    function normalizeSheetFillFields(inputData) {
+        Object.keys(sheetFillFieldsMap).forEach(function (showFieldName) {
+            if (inputData[showFieldName] === true) {
+                return;
+            }
+
+            sheetFillFieldsMap[showFieldName].forEach(function (fieldName) {
+                inputData[fieldName] = false;
+            });
         });
     }
 });
